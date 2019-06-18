ffec6fb6c29c147d7a0d019982cba545c21801bc
dogucan ozturk <dogucanozturk@hotmail.com>
2019-06-18 15:05:21 +0300

    AutoFac konfigürasyonu

--- Veresiye.UI/Program.cs
+++ Veresiye.UI/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Veresiye.Data;
+using Veresiye.Service;
 
 namespace Veresiye.UI
 {
@@ -14,6 +17,29 @@ namespace Veresiye.UI
         [STAThread]
         static void Main()
         {
+            //Autofac = IoC (Inversion-of-Control) Provider'ıdır. Bununla dependency injection işlemi kolaylaşır.
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ApplicationDbContext>().As<ApplicationDbContext>();
+
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            // servislerimiz
+            builder.RegisterType<UserService>().As<IUserService>();
+            builder.RegisterType<CompanyService>().As<ICompanyService>();
+            builder.RegisterType<ActivityService>().As<IActivityService>();
+            builder.RegisterType<FrmMain>().As<FrmMain>();
+            // formlarımız
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                var frm = scope.Resolve<FrmMain>();
+                Application.Run(frm);
+            }
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
